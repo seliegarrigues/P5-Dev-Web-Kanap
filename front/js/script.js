5e27e656a5556ec448d7b3dba44d2faa8bb441e9
Elise Garrigues <eliseg64@gmail.com>
2022-10-27 12:09:13 +0200

    ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,37 @@
-//On pense à suppr. Les Console.Log!*/
-
+//emplacement sur page accueil
+const sectionItems = document.querySelector('#items');
 //une requête HTTP avec la méthode GET, afin de récupérer des données.Appel de l'API
 fetch('http://localhost:3000/api/products')
   .then((response) => response.json())
-  .then((data) => getProducts(data))
+  .then((data) => for (const getProducts of data){
+    console.log(getProducts);
   .catch((error) => console.error(error));
 
   //création des article des produits
-  function addProducts(data) {
-    //Utilisez une boucle for  pour ajouter un par un jusqu'à atteindre 100 :
-    for (let i = 0; i < data.length; i++) {
-      let anchor = document.createElement('a');
-    document.querySelector('#items').appendChild(anchor);
-    anchor.href = `./product.html?id=${data[i]._id}`;
+  let A = document.createElement('a');
+      A.setAttribute("href", `./product.html?id=${getProducts._id}`);
+      sectionItems.appendChild(A);
+    
+    
 
     let article = document.createElement('article');
-    anchor.appendChild(article);
+    article.appendChild(article);
 
     let image = document.createElement('img');
+    image.setAttribute("src",getProducts.imageUrl);
+    image.setAttribute("alt", getProducts.altTxt);
     article.appendChild(image);
-    image.src = data[i].imageUrl;
-    image.Alt = data[i].altTxt;
+    
 
     let h3 = document.createElement('h3');
-    article.appendChild(h3);
-    h3.classList.add('productName');
-    h3.textContent = data[i].name;
+    h3.setAttribute("class","productName");
+      h3.innerText = getProducts.name;
+      article.appendChild(h3);
 
     let p = document.createElement('p');
-    article.appendChild(p);
-    p.classList.add('productDescription');
-    p.textContent = data[i].description;
+    p.setAttribute("class","productDescription");
+      p.innerText = getProducts.description;
+      article.appendChild(p);
     }
-  }
+   })
   
